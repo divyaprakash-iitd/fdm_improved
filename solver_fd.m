ba130eb5a43eed09278cd38512f683ac573575df
Divyaprakash <amz208461@am.iitd.ac.in>
2022-04-11 15:23:22 +0530

    Delegated the lines to functions

--- solver_fd.m
+++ solver_fd.m
@@ -12,7 +12,7 @@ ny = nx/ar;
 % Create grid points
 X = linspace(0,xmax,nx);
 Y = linspace(0,ymax,ny);
-[x,y] = ndgrid(X,Y);
+[x,y] = meshgrid(X,Y);
 
 % Generate a parametric closed curve
 t = linspace(0,2*pi,nx);
@@ -30,5 +30,56 @@ liquid = ~solid;
 hold on
 plot(x(liquid),y(liquid),'rx')
 plot(x(solid),y(solid),'bo')
-plot(xp,yp,'-.')
+plot(xp,yp,'k-.')
 axis off equal
+
+% Solve a dirichlet heat conduction problem
+% Boundary condition
+Tw = 10; % West
+Te = 20; % East
+Tn = 30;  % North
+Ts = 40; % South
+
+
+% Description: Solves a 2D steady state heat transfer problem with no heat
+% generation, with Dirichlet boundary conditions at the boundaries.
+
+% Constants
+N           = 5;
+LEFT        = 1;
+RIGHT       = 2;
+TOP         = 3;
+BOTTOM      = 4;
+CENTER      = 5;
+
+% Grid Size
+dx  = xmax/(nx-1);
+dy  = ymax/(ny-1);
+
+Nx  = nx;
+Ny  = ny;
+
+% Coefficient Matrix
+A = ones(Nx,Ny,N);
+A(:,:,CENTER) = -4;
+
+% RHS Matrix
+B = zeros(Nx,Ny);
+
+% Solution Matrix
+Ti = 300; % Initial Guess
+T = Ti*ones(Nx,Ny);
+
+
+% Dirichlet BC (Left and Right)
+T(:,1)      = Te;
+T(:,end)    = Tw;
+% Dirichlet BC (Top and Bottom)
+T(1,:)      = Tn;
+T(end,:)    = Ts;
+
+
+T = solve_poisson(A,B,T);
+
+% Implement a function which calculates the coefficients
+A = calculate_coefficients(x,y,xp,yp);
