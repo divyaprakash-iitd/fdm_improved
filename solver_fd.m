2d269289c4722766a4f6b5d1a67c5edbd4497be6
Divyaprakash <amz208461@am.iitd.ac.in>
2022-04-11 13:56:17 +0530

    Generates a grid with solid and liquid nodes

--- solver_fd.m
+++ solver_fd.m
@@ -1,2 +1,34 @@
 clear; clc; close all;
 
+% Generate a rectangular grid
+
+xmax = 2;
+ymax = 1;
+ar = xmax/ymax;
+
+nx = 50;
+ny = nx/ar;
+
+% Create grid points
+X = linspace(0,xmax,nx);
+Y = linspace(0,ymax,ny);
+[x,y] = ndgrid(X,Y);
+
+% Generate a parametric closed curve
+t = linspace(0,2*pi,nx);
+cx = xmax/2; cy = ymax/2;
+radius = 0.2;
+xp = radius*cos(t) + cx;
+yp = radius*sin(t) + cy;
+
+[in, on] = inpolygon(x,y,xp,yp);
+
+% Define flags
+solid = logical(in + on);
+liquid = ~solid;
+
+hold on
+plot(x(liquid),y(liquid),'rx')
+plot(x(solid),y(solid),'bo')
+plot(xp,yp,'-.')
+axis off equal
