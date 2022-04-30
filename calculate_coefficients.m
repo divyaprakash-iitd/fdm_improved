8251a12df77a26af9e9d66ee1e566f0464af2c30
Divyaprakash <amz208461@am.iitd.ac.in>
2022-04-30 14:26:36 +0530

    Tried to find intersection of the a piecewise linear curve with the grid lines

--- calculate_coefficients.m
+++ calculate_coefficients.m
@@ -1,33 +1,39 @@
 clear; clc; close all;
 
-% calculate_coefficient()
-% function A = calculate_coefficient()
-    % Generate a rectangular grid
 
-    xmax = 1;
-    ymax = 1;
-    ar = xmax/ymax;
-    
-    nx = 20;
-    ny = nx/ar;
-    
-    % Create grid points
-    X = linspace(0,xmax,nx);
-    Y = linspace(0,ymax,ny);
-    [x,y] = ndgrid(X,Y);
-    
-    % Generate a parametric closed curve
-    t = linspace(0,2*pi,10*nx);
-    cx = xmax/2; cy = ymax/2;
-    radius = 0.2;
-%     xp = radius*cos(t) + cx;
-%     yp = radius*sin(t) + cy;
+xmax = 1;
+ymax = 1;
+ar = xmax/ymax;
+
+nx = 11;
+ny = nx/ar;
+
+% Create grid points
+X = linspace(0,xmax,nx);
+Y = linspace(0,ymax,ny);
+[x,y] = ndgrid(X,Y);
+
+% Generate a parametric closed curve
+t = linspace(0,2*pi,nx);
+cx = xmax/2; cy = ymax/2;
+radius = 0.2;
+
 global a b
-a = 0.2;
-b = 0.3;
-xp = a*cos(t) + cx;
+a = 0.3;
+b = 0.45;
+xp = a* cos(t) + cx;
 yp = b*sin(t) + cy;
     
+% Generate lines
+pl = generate_lines(xp,yp);
+% figure()
+% hold on
+% for i = 1:size(pl,1)
+%     fplot(@(x)polyval(pl(i,:),x),[-1,1])
+%     pause(1)
+% end
+% return
+
     [in, on] = inpolygon(x,y,xp,yp);
     
     % Define flags
@@ -53,32 +59,34 @@ yp = b*sin(t) + cy;
                 if solid(i+1,j)
                     % Calculate the intersection point
                     p = polyfit([x(i,j),x(i+1,j)],[y(i,j),y(i,j)],1);
+                    [xi, yi] = calculate_intersection(p,pl,x(i,j));
+%                     break;
 
-                    t = atan2(y(i,j)-cy,x(i,j)-cx);
-                    [xi, yi] = calculate_intersection_point(p,t,xi,yi,radius,cx,cy);
-%                     plot(xi,yi,'mo')
-                    x(i,j)
-                   
-
-                elseif solid(i-1,j)
-                    p = polyfit([x(i,j),x(i-1,j)],[y(i,j),y(i,j)],1);
-                    t = atan2(y(i,j)-cy,x(i,j)-cx);
-                    [xi, yi] = calculate_intersection_point(p,t,xi,yi,radius,cx,cy);
-%                     xqq = linspace(x(i,j),x(i-1,j),10);
-%                     plot(xqq,polyval(p,xqq),'k-')
-%                     plot(xi,yi,'mo')
-
-                elseif solid(i,j+1)
-                    p = polyfit([x(i,j),x(i,j)],[y(i,j),y(i,j+1)],1);
-                    t = atan2(y(i,j)-cy,x(i,j)-cx);
-                    [xi, yi] = calculate_intersection_point(p,t,xi,yi,radius,cx,cy);
-%                     plot(xi,yi,'mo')
-
-                elseif solid(i,j-1)
-                    p = polyfit([x(i,j),x(i,j)],[y(i,j),y(i,j-1)],1);
-                    t = atan2(y(i,j)-cy,x(i,j)-cx);
-                    [xi, yi] = calculate_intersection_point(p,t,xi,yi,radius,cx,cy);
-%                     plot(xi,yi,'mo')
+%                     t = atan2(y(i,j)-cy,x(i,j)-cx);
+%                     [xi, yi] = calculate_intersection_point(p,t,xi,yi,radius,cx,cy);
+                    plot(xi,yi,'mo')
+%                     x(i,j)
+%                    
+% 
+%                 elseif solid(i-1,j)
+%                     p = polyfit([x(i,j),x(i-1,j)],[y(i,j),y(i,j)],1);
+%                     t = atan2(y(i,j)-cy,x(i,j)-cx);
+%                     [xi, yi] = calculate_intersection_point(p,t,xi,yi,radius,cx,cy);
+% %                     xqq = linspace(x(i,j),x(i-1,j),10);
+% %                     plot(xqq,polyval(p,xqq),'k-')
+% %                     plot(xi,yi,'mo')
+% 
+%                 elseif solid(i,j+1)
+%                     p = polyfit([x(i,j),x(i,j)],[y(i,j),y(i,j+1)],1);
+%                     t = atan2(y(i,j)-cy,x(i,j)-cx);
+%                     [xi, yi] = calculate_intersection_point(p,t,xi,yi,radius,cx,cy);
+% %                     plot(xi,yi,'mo')
+% 
+%                 elseif solid(i,j-1)
+%                     p = polyfit([x(i,j),x(i,j)],[y(i,j),y(i,j-1)],1);
+%                     t = atan2(y(i,j)-cy,x(i,j)-cx);
+%                     [xi, yi] = calculate_intersection_point(p,t,xi,yi,radius,cx,cy);
+% %                     plot(xi,yi,'mo')
 
                     
                 end
@@ -87,6 +95,33 @@ yp = b*sin(t) + cy;
     end
 % end
 plot(xi,yi,'mo')
+
+
+function p = generate_lines(xp,yp)
+    % Generate lines connecting points
+    p = zeros(numel(xp)-1,2);
+    
+    for i = 1:numel(xp)-1
+        p(i,:) = polyfit([xp(i), xp(i+1)],[yp(i), yp(i+1)],1);
+    end
+end
+
+function [xi, yi] = calculate_intersection(p,pl,xo)
+    global a b
+
+    % Check for intersection of the given line with the parametric curve
+
+%     xi = nan;
+    for i = 1:size(pl,1)
+%         if isnan(xi)
+            pfun = @(x) polyval(pl(i,:)-p,x);
+            xi = fzero(pfun,xo)
+%             break;
+%         end
+    end
+    yi = polyval(p,xi);
+
+end
 
 function [xi, yi] = calculate_intersection_point(p,xo,xi,yi,r,cx,cy)
 global a b
